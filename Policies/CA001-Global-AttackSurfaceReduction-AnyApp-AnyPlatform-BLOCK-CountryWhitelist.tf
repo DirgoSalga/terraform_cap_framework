91af0047dd1d4fd99b396afa568c586d90ca8a76
Diego Salgado <diego.salgado@dirgosalga.com>
2026-06-13 21:39:00 +0200

    Refactor excluded groups and locations in CA001 policy

--- Policies/CA001-Global-AttackSurfaceReduction-AnyApp-AnyPlatform-BLOCK-CountryWhitelist.tf
+++ Policies/CA001-Global-AttackSurfaceReduction-AnyApp-AnyPlatform-BLOCK-CountryWhitelist.tf
@@ -20,15 +20,15 @@ resource "azuread_conditional_access_policy" "ca001" {
 
     users {
       included_users = ["All"]
-      excluded_groups = concat(
-        ["6499f521-8620-4f4e-92a1-db47c79362e8", "2802b872-ccfb-4b29-a9a9-459808dfb11b", "813e2655-e8b9-4255-91f5-7761ee2824bb"],
-        [azuread_group.ca001_exclusion.object_id]
-      )
+      excluded_groups = [
+        azuread_group.breakglass.object_id,
+        azuread_group.ca001_exclusion.object_id
+      ]
     }
 
     locations {
       included_locations = ["All"]
-      excluded_locations = ["185c993e-10a9-44fa-98d1-230c8f72f497"]
+      excluded_locations = [azuread_named_location.eu.id]
     }
   }
 
